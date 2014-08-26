72516fc313712905503665e41d762a0d55e5925e
Him You Ten <himyouten@gmail.com>
2014-08-25 21:31:55 -0400

    added purgeRelated() using redis

--- test/test-nginx-cache-kill.js
+++ test/test-nginx-cache-kill.js
@@ -4,13 +4,19 @@ describe('nginx-cache-kill', function(){
     var assert = require("assert")
     var config = require('../config/config');
     var logger = require('../config/log')(config);
+    var redis = require('redis');
 
     var tmpDir = './test/tmp';
     var testConfig = {
         cachekill: {
             default: {
                 cache_levels: '1:2',
-                cache_dir: '/var/spool/nginx'
+                cache_dir: '/var/spool/nginx',
+                has_related: false,
+                redis: {
+                    host: '127.0.0.1',
+                    port: 6379
+                }
             },
             sites: {
                 'my.domain': {
@@ -20,6 +26,11 @@ describe('nginx-cache-kill', function(){
                 'my.test': {
                     cache_levels: '',
                     cache_dir: tmpDir
+                },
+                'my.testRelated': {
+                    cache_levels: '',
+                    cache_dir: tmpDir,
+                    has_related: true,
                 }
             }
         }
@@ -27,6 +38,33 @@ describe('nginx-cache-kill', function(){
 
     config.overrides(testConfig);
     var nginxCache = new NginxCacheKill(config.get('cachekill:sites'), config.get('cachekill:default'));
+    var redisClient = null;
+    try {
+        
+        logger.log('info','connecting to redis %s:%s', testConfig.cachekill.default.redis.host, testConfig.cachekill.default.redis.port);
+        // var redisClient = redis.createClient(testConfig.cachekill.default.redis.port, testConfig.cachekill.default.redis.host);
+        redisClient = redis.createClient();
+        
+        logger.log('info', 'connected to %s:%s', redisClient.host, redisClient.port);
+        redisClient.on("error", function(err){
+            logger.log('error', 'redis error:'+err)
+        });
+        redisClient.on("connect", function(){
+            logger.log('info', 'test redis connected');
+            redisClient.sadd(url, 'http://my.testrelated/purgeTest.htm?related1');
+            redisClient.sadd(url, 'http://my.testrelated/purgeTest.htm?related2');
+            redisClient.smembers(url, function(err, urls){
+                if (err){ 
+                    logger.log('error', 'error fetching related urls, '+err);
+                    return;
+                }
+                logger.log('info', 'related urls added: '+urls);
+            });
+        });
+
+    } catch (err){
+        logger.log('warn', 'cannot connect to redis');
+    }
     
     describe('#getConfig()', function(){
         
@@ -90,8 +128,8 @@ describe('nginx-cache-kill', function(){
     });
     
     describe('#purgeUrl()', function(){
-        var url = 'http://my.test/some/path';
-        var urlhash = 'df8b3d7630cb66b494a112cc162e274e';
+        var url = 'http://my.test/purgeUrlTest.htm';
+        var urlhash = 'a8eaceed55be868b1b7058c37cc12f76';
 
         beforeEach(function(){
             try {
@@ -109,9 +147,66 @@ describe('nginx-cache-kill', function(){
         it('should remove the cache file', function(done){
             assert.ok(fs.statSync(tmpDir + '/' + urlhash).isFile(), 'missing test hash file');
             nginxCache.purgeUrl(url);
+            logger.log('info', 'file %s isExists:%s', tmpDir + '/' + urlhash, fs.existsSync(tmpDir + '/' + urlhash));
             assert.equal(fs.existsSync(tmpDir + '/' + urlhash), false, 'test hash file not deleted');
             done();
-        })
-        ;
+        });
     });
+    
+    describe('#purge()', function(){
+        var url = 'http://my.test/purgeTest.htm';
+        var urlhash = '3a7fe5ce0ce354896335e1517f013749';
+
+        beforeEach(function(){
+            try {
+                fs.mkdirSync(tmpDir)
+            } catch (err){
+                logger.log('warn', 'failed to mkdir tmpDir:%s, %s', tmpDir, err);
+            }
+            try {
+                var fd = fs.writeFileSync(tmpDir + '/' + urlhash, '');
+            } catch (err){
+                logger.log('warn', 'failed to create tmp file:%s, %s', tmpDir + '/' + urlhash, err);
+            }
+        });
+        
+        it('should remove the cache file and related from redis', function(done){
+            assert.ok(fs.statSync(tmpDir + '/' + urlhash).isFile(), 'missing test hash file');
+            nginxCache.purge(url);
+            logger.log('info', 'file %s isExists:%s', tmpDir + '/' + urlhash, fs.existsSync(tmpDir + '/' + urlhash));
+            assert.equal(fs.existsSync(tmpDir + '/' + urlhash), false, 'test hash file not deleted');
+            done();
+        });
+    });    
+    
+    describe('#purgeRelated()', function(){
+        var url = 'http://my.testrelated/purgeTest.htm';
+        // var urlhash = '3f1e26881695e5719c81e0cfca1b38cb';
+        var urlhashRelated1 = '40746ac599f61aa83c594def3a6d541f';
+        var urlhashRelated2 = '6a96d2706d02f82d8f7ca6324c050f6d'
+        beforeEach(function(){
+            try {
+                fs.mkdirSync(tmpDir)
+            } catch (err){
+                logger.log('warn', 'failed to mkdir tmpDir:%s, %s', tmpDir, err);
+            }
+            try {
+                // fs.writeFileSync(tmpDir + '/' + urlhash, '');
+                fs.writeFileSync(tmpDir + '/' + urlhashRelated1, '');
+                fs.writeFileSync(tmpDir + '/' + urlhashRelated2, '');
+            } catch (err){
+                logger.log('warn', 'failed to create tmp files in:%s, %s', tmpDir, err);
+            }
+        });
+
+        it('should remove the cache file and related from redis', function(done){
+            assert.ok(fs.statSync(tmpDir + '/' + urlhashRelated1).isFile(), 'missing test hash related1 file');
+            assert.ok(fs.statSync(tmpDir + '/' + urlhashRelated2).isFile(), 'missing test hash related2 file');
+            nginxCache.purgeRelated(url);
+            logger.log('info', 'file %s isExists:%s', tmpDir + '/' + urlhashRelated1, fs.existsSync(tmpDir + '/' + urlhashRelated1));
+            assert.equal(fs.existsSync(tmpDir + '/' + urlhashRelated1), false, 'test hash related1 file not deleted');
+            assert.equal(fs.existsSync(tmpDir + '/' + urlhashRelated2), false, 'test hash related2 file not deleted');
+            done();
+        });
+    });    
 })
